620f29aa0695fa59558c08f90c9ca3b7ef48a56b
Zaki Orya <zakioria@gmail.com>
2022-11-14 13:50:51 +0300

    2

--- src/pages/campgrounds/infinite.tsx
+++ src/pages/campgrounds/infinite.tsx
@@ -127,7 +127,6 @@ const Infinite: NextPage = () => {
       <div>
         <button
           onClick={() => {
-            //@ts-ignore
             skip = data?.pageParams.length!;
             return fetchNextPage();
           }}
