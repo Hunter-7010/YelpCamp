82876f025afc2041ab30657b3b63869b6e7f1127
Zaki Orya <zakioria@gmail.com>
2023-04-05 18:14:42 +0300

    added sidbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,70 +2,110 @@ import Link from "next/link";
 
 import { signIn, signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
+
+import SideBar from "./sidebar";
+
 const NavBar: React.FC = () => {
   const { data: sessionData } = useSession();
   const router = useRouter();
 
   return (
-    <header className="relative flex h-20 w-screen flex-wrap items-center justify-between bg-gradient-to-tl from-sky-500 to-indigo-500 font-sans">
-      <div className="ml-32 space-x-4">
+    <header aria-label="Site Header" className="bg-white dark:bg-gray-900">
+      <div className="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
         <Link href="/">
-          <a className="ml-2 text-3xl font-bold text-white hover:cursor-pointer">
-            YelpCamp
-          </a>
-        </Link>
-
-        <Link href="/campgrounds">
-          <span
-            className={`text-md  hover:cursor-pointer  hover:text-gray-400 ${
-              router.route === "/campgrounds" ? "text-gray-400" : "text-white"
-            }`}
-          >
-            Campgrounds
-          </span>
-        </Link>
-
-        {sessionData?.user ? (
-          <Link href="/campgrounds/new">
-            <span
-              className={`text-md hover:cursor-pointer hover:text-white ${
-                router.route === "/campgrounds/new"
-                  ? "text-gray-400"
-                  : "text-white"
-              }`}
+          <a className="block text-teal-600 dark:text-teal-300">
+            <span className="sr-only">Home</span>
+            <svg
+              className="h-8"
+              viewBox="0 0 28 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              New
-            </span>
-          </Link>
-        ) : null}
-        <Link href="/campgrounds/infinite">
-          <a
-            className={`text-sm  text-white  hover:cursor-pointer hover:text-gray-400`}
-          >
-            &#9888;Infinite camps&#9888;
+              <path
+                d="M0.41 10.3847C1.14777 7.4194 2.85643 4.7861 5.2639 2.90424C7.6714 1.02234 10.6393 0 13.695 0C16.7507 0 19.7186 1.02234 22.1261 2.90424C24.5336 4.7861 26.2422 7.4194 26.98 10.3847H25.78C23.7557 10.3549 21.7729 10.9599 20.11 12.1147C20.014 12.1842 19.9138 12.2477 19.81 12.3047H19.67C19.5662 12.2477 19.466 12.1842 19.37 12.1147C17.6924 10.9866 15.7166 10.3841 13.695 10.3841C11.6734 10.3841 9.6976 10.9866 8.02 12.1147C7.924 12.1842 7.8238 12.2477 7.72 12.3047H7.58C7.4762 12.2477 7.376 12.1842 7.28 12.1147C5.6171 10.9599 3.6343 10.3549 1.61 10.3847H0.41ZM23.62 16.6547C24.236 16.175 24.9995 15.924 25.78 15.9447H27.39V12.7347H25.78C24.4052 12.7181 23.0619 13.146 21.95 13.9547C21.3243 14.416 20.5674 14.6649 19.79 14.6649C19.0126 14.6649 18.2557 14.416 17.63 13.9547C16.4899 13.1611 15.1341 12.7356 13.745 12.7356C12.3559 12.7356 11.0001 13.1611 9.86 13.9547C9.2343 14.416 8.4774 14.6649 7.7 14.6649C6.9226 14.6649 6.1657 14.416 5.54 13.9547C4.4144 13.1356 3.0518 12.7072 1.66 12.7347H0V15.9447H1.61C2.39051 15.924 3.154 16.175 3.77 16.6547C4.908 17.4489 6.2623 17.8747 7.65 17.8747C9.0377 17.8747 10.392 17.4489 11.53 16.6547C12.1468 16.1765 12.9097 15.9257 13.69 15.9447C14.4708 15.9223 15.2348 16.1735 15.85 16.6547C16.9901 17.4484 18.3459 17.8738 19.735 17.8738C21.1241 17.8738 22.4799 17.4484 23.62 16.6547ZM23.62 22.3947C24.236 21.915 24.9995 21.664 25.78 21.6847H27.39V18.4747H25.78C24.4052 18.4581 23.0619 18.886 21.95 19.6947C21.3243 20.156 20.5674 20.4049 19.79 20.4049C19.0126 20.4049 18.2557 20.156 17.63 19.6947C16.4899 18.9011 15.1341 18.4757 13.745 18.4757C12.3559 18.4757 11.0001 18.9011 9.86 19.6947C9.2343 20.156 8.4774 20.4049 7.7 20.4049C6.9226 20.4049 6.1657 20.156 5.54 19.6947C4.4144 18.8757 3.0518 18.4472 1.66 18.4747H0V21.6847H1.61C2.39051 21.664 3.154 21.915 3.77 22.3947C4.908 23.1889 6.2623 23.6147 7.65 23.6147C9.0377 23.6147 10.392 23.1889 11.53 22.3947C12.1468 21.9165 12.9097 21.6657 13.69 21.6847C14.4708 21.6623 15.2348 21.9135 15.85 22.3947C16.9901 23.1884 18.3459 23.6138 19.735 23.6138C21.1241 23.6138 22.4799 23.1884 23.62 22.3947Z"
+                fill="currentColor"
+              />
+            </svg>
           </a>
         </Link>
-      </div>
-      {sessionData?.user ? (
-        <div className="mr-32 space-x-2">
-          <span className="text-md mx-2 select-none uppercase text-white">
-            {sessionData?.user?.name}
-          </span>
-          <button
-            className="text-lg font-bold text-gray-300 hover:text-white"
-            onClick={() => signOut()}
-          >
-            Sign out
-          </button>
+
+        <div className="flex flex-1 items-center justify-end md:justify-between">
+          <nav aria-label="Site Nav" className="hidden md:block">
+            <ul className="flex items-center gap-6 text-sm">
+              <li>
+                <Link href="/campgrounds">
+                  <a
+                    className={`text-md transition hover:cursor-pointer hover:text-gray-500/75 dark:hover:text-white/75  ${
+                      router.route === "/campgrounds"
+                        ? "text-gray-800 dark:text-white hover:text-current"
+                        : "text-gray-500 dark:text-gray-300"
+                    }`}
+                  >
+                    Campgrounds
+                  </a>
+                </Link>
+              </li>
+              {sessionData?.user && (
+                <li>
+                  <Link href="/campgrounds/new">
+                    <a
+                      className={`text-md transition hover:cursor-pointer hover:text-gray-500/75 dark:hover:text-white/75  ${
+                        router.route === "/campgrounds/new"
+                          ? "text-gray-800 dark:text-white hover:text-current"
+                          : "text-gray-500 dark:text-gray-300"
+                      }`}
+                    >
+                      +Add Camp
+                    </a>
+                  </Link>
+                </li>
+              )}
+
+              <li>
+                <Link href="/campgrounds/infinite">
+                  <a
+                    className={`text-md transition hover:cursor-pointer hover:text-gray-500/75 dark:hover:text-white/75  ${
+                      router.route === "/campgrounds/infinite"
+                        ? "text-gray-800 dark:text-white hover:text-current"
+                        : "text-gray-500 dark:text-gray-300"
+                    }`}
+                  >
+                    All Campgrounds
+                  </a>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+
+          <div className="flex items-center gap-4">
+            <div className="sm:flex sm:gap-4">
+              {sessionData?.user ? (
+                
+                  <a
+                    title="sign out"
+                    className="cursor-pointer rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600 transition hover:text-teal-600/75 dark:bg-gray-800 dark:text-white dark:hover:text-white/75"
+                    onClick={() => signOut()}
+                  >
+                    Sign out
+                  </a>
+                
+              ) : (
+                <a
+                  className="block cursor-pointer rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-teal-700 dark:hover:bg-teal-500"
+                  title="sign in with Google"
+                  onClick={() => signIn("google")}
+                >
+                  Login
+                </a>
+              )}
+            </div>
+
+           <SideBar>
+            <div>hello</div>
+           </SideBar>
+          </div>
         </div>
-      ) : (
-        <button
-          className="mr-32 text-lg font-bold text-white hover:text-gray-400"
-          onClick={() => signIn("google")}
-        >
-          Sign in with Google
-        </button>
-      )}
+      </div>
     </header>
   );
 };
---
+++ src/components/sidebar.tsx
@@ -0,0 +1,102 @@
+import { Fragment, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+type Props = {
+  children: React.ReactNode;
+};
+export default function SideBar({ children }: Props) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <button
+        type="button"
+        onClick={() => setOpen(!open)}
+        className="block rounded bg-gray-100 p-2.5 text-gray-600 transition hover:text-gray-600/75 dark:bg-gray-800 dark:text-white dark:hover:text-white/75 md:hidden"
+      >
+        <span className="sr-only">Toggle menu</span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth="2"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M4 6h16M4 12h16M4 18h16"
+          />
+        </svg>
+      </button>
+
+      <Transition.Root show={open} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={setOpen}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-in-out duration-500"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in-out duration-500"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-hidden">
+            <div className="absolute inset-0 overflow-hidden">
+              <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+                <Transition.Child
+                  as={Fragment}
+                  enter="transform transition ease-in-out duration-500 sm:duration-700"
+                  enterFrom="translate-x-full"
+                  enterTo="translate-x-0"
+                  leave="transform transition ease-in-out duration-500 sm:duration-700"
+                  leaveFrom="translate-x-0"
+                  leaveTo="translate-x-full"
+                >
+                  <Dialog.Panel className="pointer-events-auto w-screen max-w-md bg-white dark:bg-gray-800 dark:text-gray-200">
+                    <div className="flex h-full flex-col overflow-y-scroll shadow-xl">
+                      <div className="flex-1 overflow-y-auto py-6 px-4 sm:px-6">
+                        <div className="flex items-start justify-between">
+                          <Dialog.Title className="text-lg font-medium text-gray-900 dark:text-gray-50">
+                            YelpCamp
+                          </Dialog.Title>
+                          <div className="ml-3 flex h-7 items-center">
+                            <button
+                              type="button"
+                              className="-m-2 p-2 text-gray-400 hover:text-gray-500 dark:text-gray-50 dark:hover:text-gray-400"
+                              onClick={() => setOpen(false)}
+                            >
+                              <span className="sr-only">Close panel</span>
+                              <svg
+                                className={`${
+                                  open ? "block" : "hidden"
+                                } h-6 w-6`}
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                              >
+                                <path d="M6 18L18 6M6 6l12 12"></path>
+                              </svg>
+                            </button>
+                          </div>
+                        </div>
+
+                        <div className="mt-8">
+                          <div className="flow-root">{children}</div>
+                        </div>
+                      </div>
+                    </div>
+                  </Dialog.Panel>
+                </Transition.Child>
+              </div>
+            </div>
+          </div>
+        </Dialog>
+      </Transition.Root>
+    </>
+  );
+}
